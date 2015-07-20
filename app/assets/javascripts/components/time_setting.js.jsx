793ab5750821497c2523dfe22c4b82f7b16f46bf
Takayuki Shimizu <t.shimizu0713@gmail.com>
2015-07-20 20:41:14 +0900

    add header to panels.

--- app/assets/javascripts/components/time_setting.js.jsx
+++ app/assets/javascripts/components/time_setting.js.jsx
@@ -6,8 +6,10 @@ var TimeSetting = React.createClass({
     var month = ('0' + (today.getMonth() + 1)).slice(-2);
     var day = ('0' + today.getDate()).slice(-2);
     return (
-      <div>
+      <div className="panel">
+        <div className='panel-heading'>
         {year}/{month}/{day} Start:{this.props.setting.start_time} End:{this.props.setting.end_time} rest:{this.props.setting.rest} h work:{this.props.setting.end_time - this.props.setting.start_time - this.props.setting.rest} h
+        </div>
       </div>
     );
   }
