a77db212b88d98ce1a8b95175f85be77dbfd5182
Takayuki Shimizu <t.shimizu0713@gmail.com>
2015-07-27 02:14:19 +0900

    include chartjs.

--- app/assets/javascripts/components/bar_charts.js.jsx
+++ app/assets/javascripts/components/bar_charts.js.jsx
@@ -5,11 +5,27 @@ var BarCharts = React.createClass({
         <div className='panel'>
           <header className='panel-heading'>working hours</header>
           <div className='panel-body'>
-            estimate: {this.props.estimated_working_hours} h / {this.props.working_hours} h
-            <br/>
-            Actual: {this.props.actual_working_hours} h / {this.props.working_hours} h
-            <br/><br/>
+            <h5>Daily</h5>
+            <div className=''>
+              <div className='pull-left'>
+                <DoughnutChart/>
+              </div>
+              <div className=''>
+                estimate: {this.props.estimated_working_hours} h / {this.props.working_hours} h
+                <br/>
+                Actual: {this.props.actual_working_hours} h / {this.props.working_hours} h
+                <br/><br/>
+              </div>
+            </div>
           </div>
+          <ul className="list-group">
+            <li className="list-group-item">
+              <h5>Weekly</h5>
+              <BarChart
+                estimated={this.props.estimated_working_hours}  actual={this.props.actual_working_hours}
+              />
+            </li>
+          </ul>
         </div>
       </div>
     );
